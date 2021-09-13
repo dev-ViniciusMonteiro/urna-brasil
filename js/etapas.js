1b14722e4f4b62d243900ca3395567ea5e4fd702
Vinicius Monteiro <dev.viniciusmonteiro@gmail.com>
2021-09-13 18:01:00 -0300

    Add files via upload

---
+++ js/etapas.js
@@ -0,0 +1,27 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+            numero: '00000',
+            nome: 'Vinicius Monteiro',
+            partido: 'Partido Escolar - PE -Planos: rejuste salarial, Plano novas escolas e aumento salarial',
+            fotos: [
+                { url: 'eu.jpg', legenda: 'Vereador' }
+            ]
+        }, ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+            numero: '00',
+            nome: 'DR Vinicius Monteiro',
+            partido: 'Partido Escolar - PE',
+            vice: 'Braian OCorno',
+            fotos: [
+                { url: 'eu.jpg', legenda: 'Prefeito' },
+                { url: 'eu.jpg', legenda: 'Vice-Prefeito', small: true }
+            ]
+        }, ]
+    }
+];
